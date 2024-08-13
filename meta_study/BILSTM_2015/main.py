41b5f517fd65eea977df05c2e55c317022257a52
Muhammad Zakriya <zakriyaahmad6@gmail.com>
2024-08-13 14:49:52 +0200

    BILSTM training change/more

--- meta_study/BILSTM_2015/main.py
+++ meta_study/BILSTM_2015/main.py
@@ -7,18 +7,18 @@ import utils
 
 if __name__=='__main__':
 
-    #a = Train('/home/ann_ss22_group4/step detection/SIMUL-dataset/data/by-person/train','cuda',0.001,30)
-    #a.train()
-    #a.save_model()
+    a = Train('/home/ann_ss22_group4/step detection/SIMUL-dataset/data/by-person/train','cuda',0.01,30)
+    a.train()
+    a.save_model()
 
     b = Test('/home/ann_ss22_group4/step detection/SIMUL-dataset/data/by-person/test','cuda',0.001,30)
-    start,end = b.test()
+    start,end,_ = b.test()
     #print(start_with_patches.shape)
     #clean_start = utils.remove_small_brusts(start_with_patches,10)
     #clean_end = utils.remove_small_brusts(end_with_patches,10)
 
-    start = utils.remove_patches(start)
-    end = utils.remove_patches(end)
+    #start = utils.remove_patches(start)
+    #end = utils.remove_patches(end)
     df = {'start':start,'end':end}
     df = pd.DataFrame(df)
 
--- meta_study/BILSTM_2015/train.py
+++ meta_study/BILSTM_2015/train.py
@@ -1,8 +1,10 @@
+import os
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-
+import numpy as np
 
 from loss import BCEWithWeights
 from model import BiLSTMModel as LSTM
@@ -20,6 +22,10 @@ class Train():
         self.dataset = LSTMDataloader(self.root,ToFilter=True,AddMagnitude=True,AddGyro=False,normalize=True,window_size=500,stride=1,windowed_labels=True)
         self.dataloader = DataLoader(self.dataset,batch_size=1024,shuffle=True)
         self.loss_history_epoch = []
+        self.acc_history_epoch = []
+        files = os.listdir()
+        if 'BILSTM_2015_30_epochs.pt' in files:
+            self.model = torch.load('BILSTM_2015_30_epochs.pt')
 
     def train(self):
         self.model = self.model.train().to(self.device)
@@ -29,27 +35,39 @@ class Train():
 
         criterion = BCEWithWeights(self.ws0,self.we0,self.ws1,self.we1,self.device)
 
+        try:
+            for epoch in range(self.epochs):
+                epoch_loss = 0.0
+                for batch_input,batch_label in self.dataloader:
+                    
+                    optimizer.zero_grad()
+                    batch_input,batch_label = batch_input.to(self.device).float(),batch_label.to(self.device)
 
-        for epoch in range(self.epochs):
-            epoch_loss = 0.0
-            for batch_input,batch_label in self.dataloader:
-                
-                optimizer.zero_grad()
-                batch_input,batch_label = batch_input.to(self.device).float(),batch_label.to(self.device)
+                    outputs = self.model(batch_input)
+                    #print(batch_input.shape,outputs.shape,batch_label.shape)
+                    loss = criterion(outputs,batch_label)
 
-                outputs = self.model(batch_input)
-                #print(batch_input.shape,outputs.shape,batch_label.shape)
-                loss = criterion(outputs,batch_label)
+                    loss.backward()
+                    optimizer.step()
+                    epoch_loss += loss.item()
 
-                loss.backward()
-                optimizer.step()
-                epoch_loss += loss.item()
+                avg_loss = epoch_loss/len(self.dataloader)
+                print(f'Epoch: {epoch},    Loss: {avg_loss}')
+                self.loss_history_epoch.append(avg_loss)
 
-            avg_loss = epoch_loss/len(self.dataloader)
-            print(f'Epoch: {epoch},    Loss: {avg_loss}')
-            self.loss_history_epoch.append(avg_loss)
-
-        return self.model,self.loss_history_epoch
+                if epoch%10 == 0:
+                    self.save_model()
+                    b = Test('/home/ann_ss22_group4/step detection/SIMUL-dataset/data/by-person/test','cuda',0.001,30)
+                    start,_,Tstart = b.test()
+                    acc = b.count_accuracy(start,Tstart)
+                    self.acc_history_epoch.append(acc)
+                    print(f'Epoch: {epoch},    Acc: {acc}')
+            return self.model,self.loss_history_epoch
+        except:
+            print('Interrupted. Saving model')
+            np.save('loss.npy',np.array(self.loss_history_epoch))
+            np.save('Acc.npy',np.array(self.acc_history_epoch))
+            self.save_model()
 
 
     def save_model(self):
@@ -74,7 +92,7 @@ class Test():
     def test(self):
         sigmoid = nn.Sigmoid()
         start = True
-        print(len(self.dataloader))
+        #print(len(self.dataloader))
         for test_input,test_label in self.dataloader:
             if start:
                 #print(test_input.shape)
@@ -85,11 +103,11 @@ class Test():
                 logits_S,logits_E = output[0,:,0],output[0,:,1]
                 logits_S, logits_E = sigmoid(logits_S),sigmoid(logits_E)
                 #print(output.shape,logits_E.shape)
-                logits_S[logits_S>=0.8] == 1
-                logits_S[logits_S<0.8] == 0
+                logits_S[logits_S>=0.5] = 1
+                logits_S[logits_S<0.5] = 0
 
-                logits_E[logits_E>=0.8] == 1
-                logits_E[logits_E<0.8] == 0
+                logits_E[logits_E>=0.5] = 1
+                logits_E[logits_E<0.5] = 0
 
                 start = False
 
@@ -105,19 +123,25 @@ class Test():
                 logits_S,logits_E = output[0,:,0],output[0,:,1]
                 logits_S, logits_E = sigmoid(logits_S),sigmoid(logits_E)
                 #print(output.shape,logits_E.shape)
-                logits_S[logits_S>=0.8] == 1
-                logits_S[logits_S<0.8] == 0
+                logits_S[logits_S>=0.5] = 1
+                logits_S[logits_S<0.5] = 0
 
-                logits_E[logits_E>=0.8] == 1
-                logits_E[logits_E<0.8] == 0
-                
+                logits_E[logits_E>=0.5] = 1
+                logits_E[logits_E<0.5] = 0
                 start_preds = torch.cat((start_preds,logits_S.clone().cpu().detach()),dim=0)
                 end_preds = torch.cat((end_preds,logits_E.clone().cpu().detach()),dim=0)
-                all_labels = torch.cat((all_labels,test_label.clone().cpu().detach()),dim=0)
+                all_labels = torch.cat((all_labels,test_label.clone().cpu().detach()),dim=1)
 
-
-        return start_preds,end_preds
+        return start_preds,end_preds,all_labels[0]
     
+    def count_accuracy(self,start_preds,all_labels):
+        start_count = torch.count_nonzero(start_preds)
+        true_start_count = torch.count_nonzero(all_labels[:,0])
+        
+        acc = start_count/true_start_count
+
+        return acc*100
+
 #b = Train('/home/ann_ss22_group4/step detection/SIMUL-dataset/data/by-person/train','cuda',0.001,30)
 #b.train()
 #b.save_model()
